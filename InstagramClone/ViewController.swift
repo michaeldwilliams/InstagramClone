2986af332c897cfe812dc3e0fa4d2d10d926bd43
michaeldwilliams <michael@michaeldwilliams.com>
2016-07-18 17:14:57 -0700

    added image picker and button

--- InstagramClone/ViewController.swift
+++ InstagramClone/ViewController.swift
@@ -7,12 +7,40 @@
 //
 
 import UIKit
+import Firebase
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    lazy var imagePicker:UIImagePickerController = {
+        var image = UIImagePickerController()
+        image.delegate = self
+        image.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        image.allowsEditing = false
+        return image
+    }()
+    
+    @IBOutlet weak var importedImage: UIImageView!
+    
+    @IBAction func importImage(sender: UIButton) {
+        self.presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        importedImage.image = image
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
